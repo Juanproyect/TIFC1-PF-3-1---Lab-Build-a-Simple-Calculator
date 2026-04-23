01458632f01c72d4b7c2c454bd8804f11a130ddf
JJEchavarria <jechatab88@gmail.com>
2026-04-23 16:08:36 -0500

    Se realiza laboratorio 2 de python, pasa test

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+numero_1 = input("ingrese un numero: ")
+numero_1 = int(numero_1)
+numero_2 = input("ingrese otro numero: ") 
+numero_2 = int(numero_2)
+
+suma = numero_1 + numero_2
+resta = numero_1 - numero_2
+multiplicacion = numero_1 * numero_2
+division = numero_1 / numero_2
+
+print("la suma es: ", suma)
+print("la resta es: ", resta)
+print("la multiplicacion es: ", multiplicacion)
+print("la division es: ", division)
